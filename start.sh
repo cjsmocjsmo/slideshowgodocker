803ecf7ca36e3f4d4ffb8e124c7a76bcfc3b1f3d
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-06-16 10:14:06 -0700

    style: remove RESET_DB variable and update start.sh for architecture-specific Rust setup commands

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@ DB_PATH=/home/pimedia/go/
 IMAGE_DIR=/home/pimedia/Pictures/MASTERPICS
 IMAGE_BASE_DIR=/home/pimedia/Pictures/MASTERPICS
 HTTP_PREFIX=/static/
-RESET_DB=true
 PORT=8010
 TIMEZONE="America/Los_Angeles"
 ARCH=$(uname -m)
@@ -13,24 +12,21 @@ if [ "$ARCH" = "x86_64" ]; then
       --db-path=$DB_PATH \
       --image-dir=$IMAGE_DIR \
       --image-base-dir=$IMAGE_BASE_DIR \
-      --http-prefix=$HTTP_PREFIX \
-      --reset-db=$RESET_DB
+      --http-prefix=$HTTP_PREFIX
 elif [ "$ARCH" = "aarch64" ]; then
     echo "Running on aarch64 architecture"
     ./rust/slideshowsetuprust-linux-arm64 \
       --db-path=$DB_PATH \
       --image-dir=$IMAGE_DIR \
       --image-base-dir=$IMAGE_BASE_DIR \
-      --http-prefix=$HTTP_PREFIX \
-      --reset-db=$RESET_DB
+      --http-prefix=$HTTP_PREFIX 
 elif [ "$ARCH" = "armv7l" ]; then
     echo "Running on armv7l architecture"
     ./rust/slideshowsetuprust-linux-armv7 \
       --db-path=$DB_PATH \
       --image-dir=$IMAGE_DIR \
       --image-base-dir=$IMAGE_BASE_DIR \
-      --http-prefix=$HTTP_PREFIX \
-      --reset-db=$RESET_DB
+      --http-prefix=$HTTP_PREFIX
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
