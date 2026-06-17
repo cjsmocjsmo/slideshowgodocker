167bab5365b5b3cb66b0eff85083eaeb9202e940
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-06-16 22:30:04 -0700

    style: clean up start.sh by removing commented-out DB_PATH and DB_DIR lines

--- start.sh
+++ start.sh
@@ -1,13 +1,3 @@
-## Ex DB_PATH=/home/pimedia/go/imagesDB
-#DB_PATH=
-##Ex DB_DIR=/home/pimedia/go
-#DB_DIR=
-IMAGE_DIR=/home/pimedia/Pictures/MASTERPICS
-IMAGE_BASE_DIR=/home/pimedia/Pictures/MASTERPICS
-HTTP_PREFIX=/static/
-PORT=8010
-TIMEZONE="America/Los_Angeles"
-
 DB_PATH=/home/pimedia/go/imagesDB
 DB_DIR=/home/pimedia/go
 IMAGE_DIR=/home/pimedia/Pictures/MASTERPICS
