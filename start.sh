aa0d48a2cb37e38f9eb7ac0379425c0618faf089
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-08-15 01:32:28 -0700

    style: add missing --install flag for aarch64 and armv7l architecture setups in start.sh

--- start.sh
+++ start.sh
@@ -19,6 +19,7 @@ if [ "$ARCH" = "x86_64" ]; then
 elif [ "$ARCH" = "aarch64" ]; then
     echo "Running on aarch64 architecture"
     time ./rust/slideshowsetuprust-rpi4-0.1.0 \
+      --install \
       --db-path=$DB_PATH \
       --image-dir=$IMAGE_DIR \
       --image-base-dir=$IMAGE_BASE_DIR \
@@ -26,6 +27,7 @@ elif [ "$ARCH" = "aarch64" ]; then
 elif [ "$ARCH" = "armv7l" ]; then
     echo "Running on armv7l architecture"
     time ./rust/slideshowsetuprust-rpi3b-0.1.0 \
+      --install \
       --db-path=$DB_PATH \
       --image-dir=$IMAGE_DIR \
       --image-base-dir=$IMAGE_BASE_DIR \
